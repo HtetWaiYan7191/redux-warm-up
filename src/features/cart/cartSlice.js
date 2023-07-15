2ae95384756ebde737b38974580874bff6d7a7d8
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-07-15 11:18:29 +0630

    Install createAsyncThunk and install api in slice

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -1,12 +1,19 @@
-import { createSlice } from "@reduxjs/toolkit";
-import cartItems from "../../cartItems";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from 'axios';
+
 const initialState = {
-    cartItems: cartItems,
-    amount:cartItems.length,
+    cartItems: [],
+    amount:0,
     total:0,
     isLoading:false,
 };
 
+const URL = 'https://course-api.com/react-useReducer-cart-project';
+export const fetchCartItems = createAsyncThunk('carts/fetchCartItems', async () => {
+    const response = await axios.get(URL);
+    console.log(response)
+    return response.data;
+})
 const cartSlice = createSlice({
     name: 'cart',
     initialState,
@@ -17,11 +24,47 @@ const cartSlice = createSlice({
         },
 
         removeCart: (state, action) => {
-            console.log(action.payload)
             state.cartItems = state.cartItems.filter((item) => item.id !== action.payload);
+        },
+
+        increase: (state, action) => {
+            const cartItem = state.cartItems.find((item) => item.id === action.payload);
+            cartItem.amount = cartItem.amount + 1 ;
+        },
+
+        decrease: (state,action ) => {
+            const cartItem = state.cartItems.find((item) => item.id === action.payload);
+            cartItem.amount  = cartItem.amount - 1 ;
+        }, 
+
+        calculateTotal: (state) => {
+            let total = 0 ; 
+            let amount = 0 ;
+            state.cartItems.forEach((item) => {
+                amount += item.amount;
+                total += item.amount * item.price;
+            })
+            state.total = total;
+            state.amount = amount;
         }
     },
+    extraReducers: (builder) => {
+        builder.addCase(fetchCartItems.pending, (state) => {
+            console.log('pending')
+            state.isLoading = true;
+        });
+
+        builder.addCase(fetchCartItems.fulfilled,(state, action) => {
+            state.isLoading = false;
+            console.log(action)
+            state.cartItems = action.payload;
+        });
+
+        builder.addCase(fetchCartItems.rejected, (state) => {
+            state.isLoading = false;
+        });
+    }
 })
 
-export const {clearCart, removeCart} = cartSlice.actions;
+export const {clearCart, removeCart, increase, decrease, calculateTotal} = cartSlice.actions;
 export default cartSlice.reducer;
